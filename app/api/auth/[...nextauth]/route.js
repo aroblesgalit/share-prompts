891c6f1dbe739c440fd861587cd714b92562e9ac
Alvin Galit <aroblesgalit@gmail.com>
2023-08-08 23:09:30 -0700

    Imported database into api route

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import GoogleProvider from 'next-auth/providers/google';
+import { connectToDB } from '@utils/database';
 
 const handler = NextAuth({
   providers: [
@@ -13,9 +14,16 @@ const handler = NextAuth({
   },
   async signin({ profile }) {
     try {
-      
-    } catch (error) {
+      await connectToDB();
 
+      // check if a user already exists
+
+      // if not, create a new user
+
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
     }
   }
 });
