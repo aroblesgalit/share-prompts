4df07cf9f07767fe80349c7d123226bbe624b9d9
Alvin Galit <aroblesgalit@gmail.com>
2023-08-15 22:01:34 -0700

    Created PromptCardList component

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -3,14 +3,41 @@
 import { useState, useEffect } from 'react';
 
 import PromptCard from './PromptCard';
+import Prompt from '@models/prompt';
+
+const PromptCardList = ({ data, handleTagClick }) => {
+  return (
+    <div className="mt-16 prompt_layout">
+      {data.map((post) => (
+        <PromptCard
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+        />
+      ))}
+    </div>
+  )
+} 
 
 const Feed = () => {
   const [searchText, setSearchText] = useState('');
+  const [posts, setPosts] = useState([]);
 
   const handleSearchChange = (e) => {
 
   }
-  
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch('/api/prompt');
+      const data = await response.json();
+
+      setPosts(data);
+    }
+
+    fetchPosts();
+  }, []);
+
   return (
     <section className="feed">
       <form className='relative w-full flex-center'>
@@ -23,6 +50,11 @@ const Feed = () => {
           className='search_input peer'
         />
       </form>
+
+      <PromptCardList
+        data={posts}
+        handleTagClick={() => {}}
+      />
     </section>
   )
 }
