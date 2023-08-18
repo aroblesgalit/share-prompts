d60bea55a1e6498d60decbf04d5d9716d2fea04e
Alvin Galit <aroblesgalit@gmail.com>
2023-08-17 21:27:06 -0700

    Added image component to PromptCArd

--- models/prompt.js
+++ models/prompt.js
@@ -3,15 +3,15 @@ import { Schema, model, models } from "mongoose";
 const PromptSchema = new Schema({
   creator: {
     type: Schema.Types.ObjectId,
-    ref: 'User'
+    ref: 'User',
   },
   prompt: {
     type: String,
-    required: [true, 'Prompt is required.']
+    required: [true, 'Prompt is required.'],
   },
   tag: {
     type: String,
-    required: [true, 'Tag is required.']
+    required: [true, 'Tag is required.'],
   }
 });
 
