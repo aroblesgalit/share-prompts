79160c86aea0b48220e9a62da8302e4fb7a04105
Alvin Galit <aroblesgalit@gmail.com>
2023-08-17 21:31:01 -0700

    Added username and email to PromptCard

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -18,6 +18,15 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
             className='rounded-full object-contain'
           />
         </div>
+        
+        <div className="flex flex-col">
+          <h3 className='font-satoshi font-semibold text-gray-900'>
+            {post.creator.username}
+          </h3>
+          <p className='font-inter text-sm text-gray-500'>
+            {post.creator.email}
+          </p>
+        </div>
       </div>
     </div>
   )
