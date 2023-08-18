dd11de22a90bf71e8ae876f3dd39af0981b07fd9
Alvin Galit <aroblesgalit@gmail.com>
2023-08-17 22:50:54 -0700

    Fixed userId prop in the api new route

--- app/api/prompt/new/route.js
+++ app/api/prompt/new/route.js
@@ -2,12 +2,12 @@ import { connectToDB } from "@utils/database";
 import Prompt from '@models/prompt';
 
 export const POST = async (req) => {
-  const { uesrId, prompt, tag } = await req.json();
+  const { userId, prompt, tag } = await req.json();
 
   try {
     await connectToDB();
     const newPrompt = new Prompt({
-      creator: uesrId, 
+      creator: userId, 
       prompt,
       tag
     });
