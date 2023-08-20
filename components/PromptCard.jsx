74df3d983f821fba08ca06c72c35942a56047f10
Alvin Galit <aroblesgalit@gmail.com>
2023-08-19 19:29:11 -0700

    Created function for handling copy feature

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -7,6 +7,14 @@ import { usePathname, useRouter } from 'next/navigation';
 
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
   const [copied, setCopied] = useState('');
+
+  const handleCopy = () => {
+    setCopied(post.prompt);
+    navigator.clipboard.writeText(post.prompt);
+    setTimeout(() => {
+      setCopied('');
+    }, 3000)
+  }
   
   return (
     <div className="prompt_card">
@@ -32,7 +40,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
 
         <div 
           className="copy_btn"
-          onClick={() => {}}
+          onClick={handleCopy}
         >
           <Image
             src={copied === post.prompt
