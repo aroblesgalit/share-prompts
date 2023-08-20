6e42bac78e47c2905e3b094472626ae58d790b13
Alvin Galit <aroblesgalit@gmail.com>
2023-08-19 19:39:28 -0700

    Imported Profile component into page

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,7 +1,30 @@
-const Profile = () => {
+'use client';
+
+import { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+import Profile from '@components/profile';
+
+const MyProfile = () => {
+
+  const handleEdit = () => {
+
+  }
+
+  const handleDelete = async() => {
+
+  }
+
   return (
-    <div>Profile</div>
+    <Profile
+      name='My'
+      desc='Welcome to your personalized profile page'
+      data={[]}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
+    />
   )
 }
 
-export default Profile
+export default MyProfile
